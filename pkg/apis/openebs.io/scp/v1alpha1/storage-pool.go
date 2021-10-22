1f3c03fb7da694e1bd560d3c3554432dcd83a71f
Abhinandan Purkait <abhinandan.purkait@mayadata.io>
2021-10-22 18:04:55 +0530

    Add structure for the StoragePool  (#43)
    * add StoragePool structs for scp
    
    Signed-off-by: Abhinandan-Purkait <abhinandan.purkait@mayadata.io>
    
    * corrected StoragePoolMediaEncryptionAlgorithm type
    
    Signed-off-by: Abhinandan-Purkait <abhinandan.purkait@mayadata.io>
    
    * added comments, refined structs
    
    Signed-off-by: Abhinandan-Purkait <abhinandan.purkait@mayadata.io>
    
    * corrected some documentation
    
    Signed-off-by: Abhinandan-Purkait <abhinandan.purkait@mayadata.io>
    
    * address review comments
    
    Signed-off-by: Abhinandan-Purkait <abhinandan.purkait@mayadata.io>
    
    * removed pool specific structs, made generic
    
    Signed-off-by: Abhinandan-Purkait <abhinandan.purkait@mayadata.io>
    
    * change CR to the newest schema
    
    Signed-off-by: Abhinandan-Purkait <abhinandan.purkait@mayadata.io>
    
    * correct comments
    
    Signed-off-by: Abhinandan-Purkait <abhinandan.purkait@mayadata.io>
    
    * address review comments
    
    Signed-off-by: Abhinandan-Purkait <abhinandan.purkait@mayadata.io>
    
    * removed type field and added provisioner field along with a addition to pool states
    
    * removed finalizers constant
    
    * made provisioner and status condition fields as optional
    
    * changed provisioner variable name
    
    * removed interface type and make objectReference type as pointers
    
    * make condition inline
    
    * updated pool states
    
    * updated pool conditions
    
    * add transition states for update and delete
    
    * change UpgradePending to UpdatePending
    
    * resolve conflict
    
    Co-authored-by: Abhishek Agarwal <abhishek.agarwal@mayadata.io>

--- pkg/apis/openebs.io/scp/v1alpha1/storage-pool.go
+++ pkg/apis/openebs.io/scp/v1alpha1/storage-pool.go
@@ -17,6 +17,8 @@
 package v1alpha1
 
 import (
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -24,13 +26,159 @@ import (
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 // +resource:path=storagepool
 
+// StoragePool describes a Storage Pool resource.
 // +kubebuilder:object:root=true
-// +kubebuilder:resource:scope=Namespaced
+// +kubebuilder:resource:scope=Namespaced,shortName=sp
 type StoragePool struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
+	// Spec is the specifications and configurations to be consumed by the provisioner for a StoragePool resource
+	// +required
+	Spec StoragePoolSpec `json:"spec"`
+	// Status is for handling status of StoragePool resource
+	// +optional
+	Status StoragePoolStatus `json:"status,omitempty"`
 }
 
+// StoragePoolSpec is the spec for a StoragePool resource
+type StoragePoolSpec struct {
+	// StorageCohortReference points to the StorageCohort resource, the pool is to be created upon
+	// +required
+	StorageCohortReference *corev1.ObjectReference `json:"storageCohortReference"`
+
+	// StorageProvisioner refers to the pool provisioner which will be responsible for creating
+	// and managing the pool
+	// +optional
+	StorageProvisioner string `json:"storageProvisioner,omitempty"`
+
+	// Configuration points to the configuration, a custom resource, map of parameters or configmap
+	// that can be used to specify the pool and its device related configuration.
+	// +required
+	Configuration PoolConfiguration `json:"configuration"`
+
+	// Capabilities to be supported by the StoragePool
+	// +optional
+	Capabilities Capabilities `json:"capabilities,omitempty"`
+
+	// RequestedCapacity needed for the pool creation
+	// +optional
+	RequestedCapacity resource.Quantity `json:"requestedCapacity"`
+}
+
+// PoolConfiguration is the spec for pool to define all of its configuration
+type PoolConfiguration struct {
+	// Note: Use of both parameters and reference at the same time should be avoided.
+	// But if both are used then its upto the implementation to handle such scenario.
+
+	// Parameters are used to define all the properties or configurations of a pool.
+	// Note: This should be empty if the below Reference field is used to define the pool configurations.
+	// +optional
+	Parameters map[string]string `json:"parameters,omitempty"`
+
+	// Reference can point to either a custom resource or configmap containing all the
+	// configuration required for the pool.
+	// Note: This should be empty if the above Parameters field is used to define the pool configurations.
+	// +optional
+	Reference *corev1.ObjectReference `json:"reference,omitempty"`
+}
+
+// StoragePoolStatus is for handling status of StoragePool resource
+type StoragePoolStatus struct {
+	// ReferenceResource points to the pre-existing pool resource or the created pool resource after creation of pool.
+	// +optional
+	ReferenceResource *corev1.ObjectReference `json:"referenceResource,omitempty"`
+
+	// Capacity of the pool,viz total, used and available capacity
+	// +optional
+	Capacity StorageCapacity `json:"capacity,omitempty"`
+
+	// IOPs of the pool,viz total, provisioned, used and available IOPs
+	// +optional
+	IOPs StorageIOPs `json:"IOPs,omitempty"`
+
+	// VolumeSizeMaxLimit for maximum volume size allowed
+	// +optional
+	VolumeSizeMaxLimit resource.Quantity `json:"volumeSizeMaxLimit,omitempty"`
+
+	// Conditions enlist the conditions of the pool based on various types.
+	// +optional
+	Conditions []StoragePoolCondition `json:"state,omitempty"`
+}
+
+// StorageCapacity for the pool,viz total, used and available capacity
+type StorageCapacity struct {
+	// Total Capacity of the pool
+	// +optional
+	Total resource.Quantity `json:"total,omitempty"`
+
+	// Used Capacity of the pool
+	// +optional
+	Used resource.Quantity `json:"used,omitempty"`
+
+	// Available Capacity of the pool
+	// +optional
+	Available resource.Quantity `json:"available,omitempty"`
+
+	// Provisioned Capacity of the pool
+	// +optional
+	Provisioned resource.Quantity `json:"provisioned,omitempty"`
+}
+
+// StorageIOPs for the pool,viz total, provisioned, used and available capacity
+type StorageIOPs struct {
+	// Total IOPs of the pool
+	// +optional
+	Total uint64 `json:"total,omitempty"`
+
+	// Available IOPs of the pool
+	// +optional
+	Available uint64 `json:"available,omitempty"`
+
+	// Provisioned IOPs of the pool
+	// +optional
+	Provisioned uint64 `json:"provisioned,omitempty"`
+
+	// Used IOPs of the pool
+	// +optional
+	Used uint64 `json:"used,omitempty"`
+}
+
+// StoragePoolCondition describes the condition of a StoragePool at a certain point based on a specific type
+type StoragePoolCondition struct {
+	// Type of StoragePool condition.
+	// +optional
+	Type StoragePoolConditionType `json:"type"`
+
+	// Condition represents the storage pool's current observed condition for the above type
+	Condition `json:",inline"`
+}
+
+// StoragePoolConditionType specifies the particular state that the conditions are based on.
+type StoragePoolConditionType string
+
+const (
+	// StoragePoolConditionTypePoolHealthy condition will be available when the underlying specific pool
+	// is usable.
+	StoragePoolConditionTypePoolHealthy StoragePoolConditionType = "PoolHealthy"
+
+	// StoragePoolConditionTypePoolDegraded condition will be available when the pool is
+	// partially unavailable
+	StoragePoolConditionTypePoolDegraded StoragePoolConditionType = "PoolDegraded"
+
+	// StoragePoolConditionTypeCreationPending is the transition state of a pool when the pool is
+	// initially being provisioned
+	StoragePoolConditionTypeCreationPending StoragePoolConditionType = "CreationPending"
+
+	// StoragePoolConditionTypeDeletionPending is the transition state of a pool when the pool is
+	// getting deleted
+	StoragePoolConditionTypeDeletionPending StoragePoolConditionType = "DeletionPending"
+
+	// StoragePoolConditionTypeUpgradePending is the transition state of a pool when the pool is
+	// updated
+	StoragePoolConditionTypeUpdatePending StoragePoolConditionType = "UpdatePending"
+)
+
+// StoragePoolList is a list of StoragePool resources
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 // +resource:path=storagepools
 type StoragePoolList struct {
